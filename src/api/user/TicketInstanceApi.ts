a583aba8e5678da71455b47ea92488890ea06c56
Jmodhiambo <jmodhiambo9346@gmail.com>
2025-12-08 21:54:46 +0300

    Update user ticket type and instances

---
+++ src/api/user/TicketInstanceApi.ts
@@ -0,0 +1,18 @@
+/**
+ * Ticket Instance API Module
+ */
+
+import api from '../axiosConfig';
+import type { TicketInstance } from '../../types/TicketInstance';
+
+// Get all Ticket Instances for a User
+export const getTicketInstances = async (userId: number): Promise<TicketInstance[]> => {
+  const response = await api.get<TicketInstance[]>(`users/${userId}/ticket-instances`);
+  return response.data;
+}
+
+// Get a Ticket Instance by ID
+export const getTicketInstance = async (userId: number, id: number): Promise<TicketInstance> => {
+  const response = await api.get<TicketInstance>(`users/${userId}/ticket-instances/${id}`);
+  return response.data;
+}
---
+++ src/api/user/TicketTypes.ts
@@ -0,0 +1,12 @@
+/**
+ * Ticket Types API Module
+ */
+
+import api from '../axiosConfig';
+import type { TicketType } from "../../types/TicketType";
+
+// Get all Ticket Types for an Event
+export const getTicketTypes = async (eventId: number): Promise<TicketType[]> => {
+  const response = await api.get<TicketType[]>(`events/${eventId}/ticket-types`);
+  return response.data;
+};
