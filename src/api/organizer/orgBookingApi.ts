00bdf26ccfbe299e200873201a29fab95ddab3b4
Jmodhiambo <jmodhiambo9346@gmail.com>
2025-12-13 01:10:50 +0300

    Organizer booking route

--- src/api/organizer/orgBookingApi.ts
+++ src/api/organizer/orgBookingApi.ts
@@ -5,3 +5,8 @@
 import api from '../axiosConfig';
 import type { Booking } from '../../types/Booking';
 
+// Create booking
+export const createBooking = async (eventId: number, bookingData: any): Promise<Booking> => {
+  const response = await api.post(`/events/${eventId}/bookings`, bookingData);
+  return response.data;
+};
